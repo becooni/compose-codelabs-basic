e73a558e7a698e7539b2261eae52f51686815c7d
조정흠(Junghum Cho)/App개발팀/11ST <becooni@sk.com>
2021-08-11 22:57:45 +0900

    Initial Commit

---
+++ app/src/main/java/com/becooni/compose_codelabs_basic/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.becooni.compose_codelabs_basic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.becooni.compose_codelabs_basic.ui.theme.ComposecodelabsbasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposecodelabsbasicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposecodelabsbasicTheme {
+        Greeting("Android")
+    }
+}
